b975e808695014f69d7f1960bf8e3c8e11731576
Shehan Karunaratne <shehan.karunaratne@cognitedata.com>
2022-08-19 10:46:24 +0200

    Change project name to COGNITE_PROJECT in ClientConfig

--- sample_device_code_token_refresh.py
+++ sample_device_code_token_refresh.py
@@ -32,7 +32,7 @@ def get_token():
     return authenticate_device_code(app)["access_token"]
 
 
-cnf = ClientConfig(client_name="my-special-client", project="my-project", credentials=Token(get_token))
+cnf = ClientConfig(client_name="my-special-client", project=COGNITE_PROJECT, credentials=Token(get_token))
 client = CogniteClient(cnf)
 
 print(client.iam.token.inspect())
