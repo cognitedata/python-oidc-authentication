f28fdb2243f4c2933334f7c2e267173d8b0cd08b
Gaetan H <59728818+gaetan-h@users.noreply.github.com>
2022-08-22 11:01:22 +0200

    Update sample_device_code.py

--- sample_device_code.py
+++ sample_device_code.py
@@ -27,7 +27,7 @@ def authenticate_device_code():
 
 creds = authenticate_device_code()
 
-cnf = ClientConfig(client_name="my-special-client", project=COGNITE_PROJECT, credentials=Token(creds["access_token"]))
+cnf = ClientConfig(client_name="my-special-client", project=COGNITE_PROJECT, credentials=Token(creds["access_token"]), base_url=f"https://{CDF_CLUSTER}.cognitedata.com")
 client = CogniteClient(cnf)
 
 print(client.iam.token.inspect())
